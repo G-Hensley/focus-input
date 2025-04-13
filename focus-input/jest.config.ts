2a4e68c6318ca73fd3be3b48d95da0c1e6d993b9
Gavin Hensley <gavinhensley@protonmail.com>
2025-04-13 13:24:19 -0400

    Fix unit testing config file

---
+++ focus-input/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default createJestConfig(config);
